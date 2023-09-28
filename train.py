727bf1e260382b8316c2ee5718d329dc0392d099
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-09-28 17:22:21 +0200

    ready for training

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
 
 import config
 from dataset import NYUv2DataModule
 from model import DeepLab
 
 if __name__ == '__main__':
+
+    # Initialize the logger
+    logger = TensorBoardLogger('tb_logs/', name='nyuv2_v0')
     
     # Initialize the model
     model = DeepLab()
@@ -13,7 +17,7 @@ if __name__ == '__main__':
     data_module = NYUv2DataModule(batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS)
 
     # Initialize the trainer
-    trainer = pl.Trainer(max_epochs=config.NUM_EPOCHS, accelerator='gpu', devices=config.DEVICES)
+    trainer = pl.Trainer(logger=logger, max_epochs=config.NUM_EPOCHS, accelerator='gpu', devices=config.DEVICES)
 
     # Train the model
     trainer.fit(model, data_module)
