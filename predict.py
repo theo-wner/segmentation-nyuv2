c9ecad4e435d875b86dd1d0775a57184dbc6c6b3
theo-wner <theodor.kapler@student.kit.edu>
2023-10-11 11:40:27 +0200

    changed test to predict, runnable on cpu now

--- predict.py
+++ predict.py
@@ -1,34 +1,29 @@
 import torch
 from model import DeepLab
 from dataset import NYUv2Dataset
+
 from utils import visualize_img_gt_pr, map_40_to_13
 
 """
 Tests the model
 """
 
-# Initialize the model
+# Initialize the model and loads it to the CPU
 model = DeepLab()
-model = model.load_from_checkpoint('tb_logs/nyuv2_40_classes/version_1/checkpoints/epoch=376-step=37700.ckpt')
+checkpoint = torch.load("epoch=1999-step=200000.ckpt", map_location=torch.device('cpu'))
+model.load_state_dict(checkpoint["state_dict"])
+model.eval()
 
 # Dataset
 dataset = NYUv2Dataset(split='test')
 
+# Predict
 for i in range(50):
     image, mask = dataset[i]
 
-    # Push image to GPU
-    image = image.cuda(1)
-
-    # Prediction
-    model.eval()
     with torch.no_grad():
         pred = model(image.unsqueeze(0))
         pred = torch.argmax(pred, dim=1)
-
-    # Push to CPU
-    image = image.cpu()
-    pred = pred.cpu()
 
     # Visualization
     mask = map_40_to_13(mask)
